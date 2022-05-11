50ac15a70ece3b235e745a55f3334e47fd69e120
Dev-JeongDaeRi <kor216465@gmail.com>
2022-05-11 23:52:42 +0900

    31일차-고차함수map (요소 바꾸기)

--- day_31.playground/Contents.swift
+++ day_31.playground/Contents.swift
@@ -3,14 +3,18 @@ import UIKit
 let friendsArray : [String] = ["철수","영희","수잔","제임스","마크"]
 
 let myFriends = friendsArray.map { aFriend in
-    return "내 친구다 ->" + aFriend
+    return "내 친구다 -> " + aFriend
+}
+
+let bestFriendsArray : [String?] = ["철수",nil,"수잔",nil,"마크"]
+
+let myBestFriends : [String] = bestFriendsArray
+    .compactMap { $0 }
+    .map{
+        return "내 베프다 -> " + $0
 }
 
 let numbers : [Int] = [22, 33, 44, 55]
-
-//let myNumbers : [String] = numbers.map { aNumber in
-//    return "내 숫자 : \(aNumber)"
-//}
 
 let myNumbers : [String] = numbers.map {
     return "내 숫자 : \($0)"
@@ -18,13 +22,12 @@ let myNumbers : [String] = numbers.map {
 
 let myPetDictionary = ["고양이" : "야옹", "강아지" : "멍멍", "송아지" : "음애"]
 
-let myPets : [String] = myPetDictionary.map { (animal: String, cry: String) in
-    return "\(animal)의 울음소리는 \(cry) 이다"
+let myPets : [String] = myPetDictionary.map {
+    return "\($0)의 울음소리는 \($1) 이다"
 }
 
 let numberSet : Set<Int> = [1,1,1, 2,3,4,4,4,5]
 
-let myFavoriteNumbers : [Int] = numberSet.map { aNumber in
-    return aNumber * 10
+let myFavoriteNumbers : [String] = numberSet.map { aNumber in
+    return "\(aNumber * 10) 입니다."
 }
-
