a6c0fa64d576e54b1556a11f4f272032ad3e6f78
Dev-JeongDaeRi <kor216465@gmail.com>
2023-02-24 23:04:45 +0900

    42일차 콜렉션 합치기

---
+++ day_42.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// 콜렉션 - list [], set<>, dictionary[:]
+
+var myFriends = ["철수", "영희", "수잔"]
+
+let otherFriends : Set<String> = ["제임스", "존슨", "존시나"]
+
+//myFriends.append(contentsOf: otherFriends)
+
+let totalFriends = myFriends + otherFriends
+
+totalFriends
