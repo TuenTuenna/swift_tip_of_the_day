2b910f1ef4d8d458239a25be67794cffd4b8f592
Dev-JeongDaeRi <kor216465@gmail.com>
2023-03-05 12:49:07 +0900

    51일차 - struct 기본생성자

---
+++ day_51.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//struct Pet {
+//    var name: String
+////    init(){
+////        name = "호호호"
+////    }
+////    init(name : String = "댕댕이"){
+////        self.name = name
+////    }
+//}
+
+struct Pet {
+    var name: String
+}
+extension Pet {
+    init(){
+        name = "호호호"
+    }
+}
+
+let myCat = Pet(name: "하하하")
+
+//let myDog = Pet()
