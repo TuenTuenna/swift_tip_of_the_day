eaff5abe0d3a077546116a2fbca84126f4347723
Dev-JeongDaeRi <kor216465@gmail.com>
2023-07-30 13:36:35 +0900

    day_62. set intersection

---
+++ day_62.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let myFriends = ["철수", "영희", "수잔", "제시카"]
+let yourFriends = ["철수", "영희", "제니퍼", "존시나"]
+
+
+let myFriendsSet = Set(myFriends)
+let yourFriendsSet = Set(yourFriends)
+
+let sharedFriends = myFriendsSet.intersection(yourFriends)
