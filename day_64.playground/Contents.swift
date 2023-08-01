b9ab2f4522082bde6c57fc205998174ed30e87b3
Dev-JeongDaeRi <kor216465@gmail.com>
2023-08-01 12:05:46 +0900

    day_64. tuple

---
+++ day_64.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// (키: 값, 키: 값)
+var myPet : (name: String, kind: String, age: Int) = (name: "댕댕이", kind: "강아지", age: 1)
+
+var myCat : (String, String, age: Int) = ("개냥이", "고양이", age: 1)
+
+myCat.age
+
+
